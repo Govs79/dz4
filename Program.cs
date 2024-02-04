8c658bb7de5448978ee0542de09ee36bc594924e
Victor <govs79@outlook.com>
2024-02-05 00:36:33 +0300

    Задача 4

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<10)
+{
+    Console.WriteLine(a);
+}
+while (a>0)
+{
+  int Digit = a%10;
+  a/=10;
+  if (a>0)
+  {
+    Console.Write (Digit +",");
+  }  
+  else
+  {
+    Console.WriteLine(Digit);
+  }
+}
